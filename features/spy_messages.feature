75bd71e5f4a5dc55798e6c9782c611d0f4596adb
Kassidy Wall <wallkassidy@yahoo.com>
2024-12-09 13:59:10 -0700

    first final test attempt

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -5,8 +5,18 @@ So that I can chat with my spy friends like a pro.
 
 Scenario: I can successfully encode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I set "software quality" to the inputfield "#letters"
+    And I click on the element "#shift-amount"
+    And I select the option with the value "2" for element "#shift-amount"
+    And I click on the element "#submit"
+    And I pause for 200ms
+    Then I expect that element "#decoded_message" contains the text "uqhvyctg swcnkva"
 
 Scenario: I can successfully decode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I set "uqhvyctg swcnkva" to the inputfield "#letters"
+    And I select the option with the text "Decode" for element "#decoder-setting"
+    And I select the option with the value "2" for element "#shift-amount"
+    And I click on the element "#submit"
+    And I pause for 300ms
+    Then I expect that element "#decoded_message" contains the text "software quality"
--- test_brute.py
+++ test_brute.py
@@ -1,5 +1,7 @@
 import pytest
 from brute import Brute
+from unittest.mock import Mock
+import hashlib, random, string, time
 
 todo = pytest.mark.skip(reason='todo: pending spec')
 
@@ -8,11 +10,57 @@ def describe_Brute():
     @pytest.fixture
     def cracker():
         return Brute("TDD")
-
+    
+    @pytest.fixture
+    def hardest_cracker():
+        #as hard as it gets alphabet = string.ascii_letters + string.digits
+        return Brute(string.ascii_letters + string.digits)
+    
     def describe_bruteOnce():
-        # write your test cases here
-        pass
+        def it_returns_true_on_successful_attempt_with_simple(cracker):
+            c = cracker.bruteOnce("TDD")
+            assert c == True
+        def it_returns_true_on_successful_attempt_with_hard(hardest_cracker):
+            c = hardest_cracker.bruteOnce(string.ascii_letters + string.digits)
+            assert c == True        
+        def it_returns_false_on_bad_attempt_with_simple(cracker, hardest_cracker):
+            c = cracker.bruteOnce("laksdfh")
+            assert c == False
+        def it_returns_false_on_bad_attempt_with_hard(hardest_cracker):
+            c = hardest_cracker.bruteOnce("kahsdkf")
+            assert c == False
 
     def describe_bruteMany():
-        # write your test cases here
-        pass
+        def it_doesnt_allow_negative_limit(cracker):
+            c = cracker.bruteMany(limit=-2)
+            assert c == -1
+
+        def it_returns_positive_float_on_success(cracker):
+            seconds = cracker.bruteMany()
+            assert isinstance(seconds, float)
+            assert seconds > 0
+        
+        def it_returns_negative_one_on_fail(hardest_cracker):
+            fail = hardest_cracker.bruteMany()
+            assert isinstance(fail, int)
+            assert fail == -1
+        #have to mock the hash which is in bruteOnce
+        #have to mock random guess 
+        #all of them run limit times in brute many 
+        def it_uses_hash_in_bruteOnce_for_each_loop(mocker, cracker):
+            #have to mock brute once to use hash from brute many, fail it so it runs the loop limit amount of times
+            mock_fail_brute_once = mocker.patch.object(cracker, 'bruteOnce', return_value=False)
+            cracker.bruteMany(limit=3)
+            assert mock_fail_brute_once.call_count == 3
+        
+        def it_uses_random_guess_inside_brute_once_for_each_loop(mocker, cracker):
+            mock_fail_brute_once = mocker.patch.object(cracker, 'bruteOnce', return_value=False)
+            mock_random_guess = mocker.patch.object(cracker, 'randomGuess', return_value="mock_guess")
+            cracker.bruteMany(limit=3)
+            assert mock_random_guess.call_count == 3
+            assert mock_fail_brute_once.call_args_list == [mocker.call("mock_guess") for _ in range(3)]
+        
+
+            
+
+
